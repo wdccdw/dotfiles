d0a2f6a36b64abb233f69fdc46686635ce170c15
David Conner <wdc+github@doginspace.com>
2019-01-22 17:11:12 -0500

    merge

--- link_all.sh
+++ link_all.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-<<<<<<< HEAD
 stow -t ~ bash
 stow -t ~ git
 stow -t ~/.ssh ssh
-=======
-stow bash
-stow git
 stow -t ~/.config config
->>>>>>> fa3e978c39d488245669ac7c0da722969d0509e7
